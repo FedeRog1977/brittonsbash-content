b4fee8cba130514319b1ab692aec60d763c7c6da
Lewis Britton <lewisbritton23@yahoo.co.uk>
2021-12-07 22:39:41 +0000

    ..

--- Scripts/Theme.js
+++ Scripts/Theme.js
@@ -8,17 +8,7 @@ $(".switch2").on("click", function() {
     }
 });
 
-$(".switch4").on("click", function() {
-    if( $("*").hasClass("dark")) {
-        $("*").removeClass("dark");
-        $(".switch4").text("Toggle Night Mode: Disabled");
-    } else {
-        $("*").addClass("dark");
-        $(".switch4").text("Toggle Night Mode: Enabled");
-    }
-});
-
-$(".switch5").on("click", function() {
+$(".switch3").on("click", function() {
     if( $("*").hasClass("miami")) {
         $("*").removeClass("miami");
         $(".switch5").text("Toggle Miami Mode: Disabled");

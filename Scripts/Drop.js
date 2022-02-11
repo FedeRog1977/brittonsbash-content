126e4585d747be7bcab4e628ee046a2a59a2e0ef
Lewis Britton <lewisbritton23@yahoo.co.uk>
2022-02-11 13:06:44 +0000

    ..

---
+++ Scripts/Drop.js
@@ -0,0 +1,41 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const pages = document.querySelector("#pagesDropCont");
+    const social = document.querySelector("#socialDropCont");
+    const blog = document.querySelector("#blogDropCont");
+
+    document.querySelector("#blogDrop").addEventListener("click", e => {
+        e.preventDefault();
+        if (blog.classList.contains("hidden")) {
+            blog.classList.remove("hidden");
+        } else {
+	    blog.classList.add("hidden");
+	}
+    });
+
+    document.querySelector("#blogDrop2").addEventListener("click", e => {
+        e.preventDefault();
+        if (blog.classList.contains("hidden")) {
+            blog.classList.remove("hidden");
+        } else {
+	    blog.classList.add("hidden");
+	}
+    });
+
+    document.querySelector("#pagesDrop").addEventListener("click", e => {
+        e.preventDefault();
+        if (pages.classList.contains("hidden")) {
+            pages.classList.remove("hidden");
+        } else {
+	    pages.classList.add("hidden");
+	}
+    });
+
+    document.querySelector("#socialDrop").addEventListener("click", e => {
+        e.preventDefault();
+        if (social.classList.contains("hidden")) {
+            social.classList.remove("hidden");
+        } else {
+	    social.classList.add("hidden");
+	}
+    });
+});

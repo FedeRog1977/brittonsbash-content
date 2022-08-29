e4c49657839b3ca1386dee90f55c0c70758b9f20
Lewis Britton <lewisbritton23@yahoo.co.uk>
2022-08-29 21:02:39 +0100

    ..

---
+++ Scripts/Events.js
@@ -0,0 +1,1 @@
+let events = "https://raw.githubusercontent.com/FedeRog1977/BrittonsBashRC/master/JSON/Events.json";
---
+++ Scripts/Media.js
@@ -0,0 +1,1 @@
+let media = "https://raw.githubusercontent.com/FedeRog1977/BrittonsBashRC/master/JSON/Media.json";

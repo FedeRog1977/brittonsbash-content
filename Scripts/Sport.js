ba77236a058d6984b03deba76f99966046806354
Lewis Britton <lewisbritton23@yahoo.co.uk>
2022-08-30 09:12:33 +0100

    ..

--- Scripts/Sport.js
+++ Scripts/Sport.js
@@ -1,21 +1,19 @@
-let activities = "https://raw.githubusercontent.com/FedeRog1977/BrittonsBashRC/master/JSON/Sport.json";
-
 let test = document.getElementById("test");
 test.innerHTML = "Hello";
 
 function setRoadie() {
-    fetch(activities)
+    fetch(events)
         .then((resp) => {
 	    return resp.json();
 	})
 	.then((data) => {
-	    const sport = data;
+	    const events = data;
 	    let ptsTotal = document.getElementById("ptsTotal");
 	    let pts2022 = document.getElementById("pts2022");
 	    let pts2021 = document.getElementById("pts2021");
 	    let pts2020 = document.getElementById("pts2020");
-	    ptsTotal.innerHTML = sport.roadie[0].time;
-	)}
+	    ptsTotal.innerHTML = "Cheers";
+	})
 }
 
 setRoadie();
